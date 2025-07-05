ee9c637c66ba14d5b977be9b4055867b3ffb4413
Rajesh-Roshan98 <rrajeshroshan89@gmail.com>
2025-07-05 16:18:15 +0530

    Frontend is Completed

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Blog-Master-Frontend/', // 🔥 must match GitHub repo name exactly (case-sensitive)
   plugins: [react(), tailwindcss()],
 });
